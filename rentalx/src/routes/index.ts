4c1de6e8c860bbb5c625dff615ed28934eb0b548
Reinaldo Silva <reinaldo.silva.jr@outlook.com>
2021-04-03 22:43:27 -0300

    Add jwt

--- rentalx/src/routes/index.ts
+++ rentalx/src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { authenticateRoutes } from "./authenticate.routes";
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./specification.routes";
 import { usersRoutes } from "./user.routes";
@@ -12,5 +13,6 @@ console.log("teste");
 router.use("/categories", categoriesRoutes);
 router.use("/specification", specificationsRoutes);
 router.use("/users", usersRoutes);
+router.use(authenticateRoutes);
 
 export { router };
